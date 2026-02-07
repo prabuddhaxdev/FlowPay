26d8bc86cc12762a1de14e85cd7fb0bccddff453
prabuddhaxdev <prabuddha1749@gmail.com>
2026-02-07 22:06:22 +0530

    Initial commit

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MerchantApp = () => {
+  return (
+    <div>
+      <h1 className='text-blue-500'>Hi. This is merchant app</h1>
+    </div>
+  )
+}
+
+export default MerchantApp
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserApp = () => {
+  return (
+    <div>
+      <h1 className='text-green-400'>Hi. This is user app</h1>
+    </div>
+  )
+}
+
+export default UserApp
