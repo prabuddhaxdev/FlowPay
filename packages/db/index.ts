6132eae17ff54198bdf05de6f57d5c2380b31214
prabuddhaxdev <prabuddha1749@gmail.com>
2026-02-09 21:51:57 +0530

    Fix prisma imports

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,16 @@
-export { PrismaClient } from "./generated/prisma/index.js";
-import { PrismaClient } from "./generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma: ReturnType<typeof prismaClientSingleton> =
+  globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
