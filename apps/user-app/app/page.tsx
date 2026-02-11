d036b87494341fc2cd14af83f87bae1bee9739a4
prabuddhaxdev <prabuddha1749@gmail.com>
2026-02-11 23:58:13 +0530

    Add Signin/Signout functionality to user app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { Appbar } from "@flowpay/ui/Appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
 
-const UserApp = () => {
+export default function UserApp() {
+  const session = useSession();
+
   return (
     <div>
-      <h1 className='text-green-400'>Hi. This is user app</h1>
+      <h1 className="text-green-400">Hi. This is user app</h1>
+      <Appbar
+        onSignin={signIn}
+        onSignout={signOut}
+        user={session.data?.user}
+      ></Appbar>
     </div>
-  )
+  );
 }
-
-export default UserApp
