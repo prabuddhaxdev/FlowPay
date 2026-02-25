271b299ac46aadfb7178edead0eda4b029758f45
prabuddhaxdev <prabuddha1749@gmail.com>
2026-02-25 22:46:57 +0530

    Fix errors

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -12,7 +12,7 @@ export default function UserApp() {
         onSignin={signIn}
         onSignout={signOut}
         user={session.data?.user}
-      ></Appbar>
+      />
     </div>
   );
 }
--- packages/db/prisma/migrations/20260225171342_init/migration.sql
+++ packages/db/prisma/migrations/20260225171342_init/migration.sql
@@ -16,6 +16,17 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "p2pTransfer" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL,
+    "fromUserId" INTEGER NOT NULL,
+    "toUserId" INTEGER NOT NULL,
+
+    CONSTRAINT "p2pTransfer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Merchant" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
@@ -23,6 +34,19 @@ CREATE TABLE "Merchant" (
     "auth_type" "AuthType" NOT NULL,
 
     CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OnRampTransaction" (
+    "id" SERIAL NOT NULL,
+    "status" "OnRampStatus" NOT NULL,
+    "token" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "startTime" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "OnRampTransaction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -45,7 +69,19 @@ CREATE UNIQUE INDEX "User_number_key" ON "User"("number");
 CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "OnRampTransaction_token_key" ON "OnRampTransaction"("token");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Balance_userId_key" ON "Balance"("userId");
+
+-- AddForeignKey
+ALTER TABLE "p2pTransfer" ADD CONSTRAINT "p2pTransfer_fromUserId_fkey" FOREIGN KEY ("fromUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "p2pTransfer" ADD CONSTRAINT "p2pTransfer_toUserId_fkey" FOREIGN KEY ("toUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OnRampTransaction" ADD CONSTRAINT "OnRampTransaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Balance" ADD CONSTRAINT "Balance_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
